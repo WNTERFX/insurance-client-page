66da5b498fa328d1023cac531f6f03ae1f3a4b8e
WNTERFX <159611185+WNTERFX@users.noreply.github.com>
2025-04-19 15:06:02 +0800

    Created Changes for LoginForm

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,13 +1,12 @@
 import "./login.css"
-import "./images/logo.png"
+import "./images/Logo.png"
 export default function LoginForm(){
     return(
 
     <div class="container">
         <div class="login-card">
-          <div class="left-panel">
-            <img src={require('./images/logo.png')} alt="Silverstar Insurance Agency Inc." />
-            <p>Silverstar Insurance Agency Inc.</p>
+          <div class="logo-panel">
+            <img src={require('./images/Logo.png')} alt="silverstar_insurance_inc_Logo" />
           </div>
           <div class="right-panel">
             <h2>Log In to your account</h2>
@@ -16,7 +15,7 @@ export default function LoginForm(){
               <input type="email" placeholder="Enter your email" required />
               <label>Password</label>
               <input type="password" placeholder="Enter your password" required />
-    
+              <div class="password-button"></div>
               <button type="submit">Log In</button>
             </form>
           </div>
