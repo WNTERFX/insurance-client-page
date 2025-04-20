ced5884b94142337aaa1f0da2a1dbedf720ec13c
Anjo511 <holandes.133022@munoz.sti.edu.ph>
2025-04-20 13:49:41 +0800

    add test

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,22 +1,12 @@
 import "./login.css"
-import "./images/Logo.png"
-import { useNavigate } from "react-router-dom";
-
+import "./images/logo.png"
 export default function LoginForm(){
-
-    let navigate = useNavigate();
-    const route = () =>
-      {
-        let path = "/appinsurance/MainArea";
-        navigate(path);
-      }
-
     return(
-    <div class="login-container">
     <div class="container">
         <div class="login-card">
-          <div class="logo-panel">
-            <img src={require('./images/Logo.png')} alt="silverstar_insurance_inc_Logo" />
+          <div class="left-panel">
+            <img src={require('./images/logo.png')} alt="Silverstar Insurance Agency Inc." />
+            <p>Silverstar Insurance Agency Inc.</p>
           </div>
           <div class="right-panel">
             <h2>Log In to your account</h2>
@@ -25,12 +15,12 @@ export default function LoginForm(){
               <input type="email" placeholder="Enter your email" required />
               <label>Password</label>
               <input type="password" placeholder="Enter your password" required />
-              <div class="password-button"></div>
-              <button type="submit" onClick={() => route("appinsurance/MainArea")}>Log In</button>
+    
+              <button type="submit">Log In</button>
             </form>
           </div>
         </div>
     </div>
-    </div>
+        ///test 
     );
 }
