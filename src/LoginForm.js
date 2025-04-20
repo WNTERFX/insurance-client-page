2f83549485937fa4b22be757e56b01859c4fa62c
WNTERFX <159611185+WNTERFX@users.noreply.github.com>
2025-04-20 17:15:23 +0800

    Uploaded the new changes to the dev-branch

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,22 +1,13 @@
 import "./login.css"
 import "./images/logo.png"
-<<<<<<< HEAD
-=======
 import { useNavigate } from "react-router-dom";
 
->>>>>>> main
 export default function LoginForm(){
     return(
     <div class="container">
         <div class="login-card">
-<<<<<<< HEAD
-          <div class="left-panel">
-            <img src={require('./images/logo.png')} alt="Silverstar Insurance Agency Inc." />
-            <p>Silverstar Insurance Agency Inc.</p>
-=======
           <div class="logo-panel">
             <img src={require('./images/logo.png')} alt="silverstar_insurance_inc_Logo" />
->>>>>>> main
           </div>
           <div class="right-panel">
             <h2>Log In to your account</h2>
