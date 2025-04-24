5a074be8a3d5b76610b054d6dd31a8ec96f27332
WNTERFX <159611185+WNTERFX@users.noreply.github.com>
2025-04-24 19:20:17 +0800

    Added new features in nav and content-area

--- src/MainArea.js
+++ src/MainArea.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./styles/main_area-styles.css";
 import NavBar from "./NavBar";
 import Policy from "./Policy";
@@ -9,17 +9,24 @@ import Client from "./Client";
 import "./styles/client-styles.css"
 import Due from "./Due";
 import "./styles/due-styles.css"
+import React, { useState } from "react";
 import { Outlet } from 'react-router-dom';
 
 export default function MainArea() 
 {
 
+    const [isMinimized, setIsMinimized] = useState(false);
+
+    const handleMinimizeChange = (newMinimizedState) => {
+      setIsMinimized(newMinimizedState);
+    };  
+
     return (
         <div className="main-area">
             <div className="nav-area"> 
-                <NavBar /> 
+                <NavBar onMinimizeChange={handleMinimizeChange} /> 
             </div>
-            <div className="content-area"> 
+            <div className="content-area"  style={{ marginLeft: isMinimized ? "50px" : "200px" }}> 
                 <Outlet/>
             </div>
         </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,27 +1,59 @@
 import "./styles/nav-styles.css"
 import {Menu} from 'lucide-react';
 import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { LuUser } from "react-icons/lu";
+import { LuCalendarArrowUp } from "react-icons/lu";
+import { LuFolder } from "react-icons/lu";
 
-export default function NavBar() {
+export default function NavBar( {onMinimizeChange}) {
 
+  const [isMinimize, setIsMinimize] = useState(false);
+
+  const handleMinimize = () => {
+    setIsMinimize((prev) => {
+      const newState = !prev;
+      onMinimizeChange(newState); 
+      return newState;
+    });
+  };
 
   return (
-  <div className="nav-bar">
-    <div className="logo-bar">
-    <button className="nav-bar-button" >
-      <Menu className="nav-bar-icon" size={30} color="black" />
-    </button>
-    <h1 className="logo">Silverstar Insurance</h1>
+    <div className={`nav-bar ${isMinimize ? "minimize" : ""}`}>
+      <div className="logo-bar">
+        <button className="nav-bar-button" onClick={handleMinimize}> 
+        <Menu className="nav-bar-icon" size={30} color="black" />
+        </button>
+        {!isMinimize && <h1 className="logo">Silverstar Insurance</h1>}
+
     </div>
     <div className="side-bar">
       
-      <Link to="/appinsurance/MainArea/Dashboard" className="side-bar-item">Home</Link>
-      <Link to="/appinsurance/MainArea/Client" className="side-bar-item">Clients</Link>
-      <Link to="/appinsurance/MainArea/Due" className="side-bar-item">Due</Link>
-      <Link to="/appinsurance/MainArea/Policy" className="side-bar-item">Policy</Link>
-      <Link to="#" className="side-bar-item">Monthly Data</Link> 
-      <Link to="#" className="side-bar-item">Login</Link>
-       
+      <Link to="/appinsurance/MainArea/Dashboard" className="side-bar-item">
+        {isMinimize ? <LuLayoutDashboard /> : "Dashboard"}
+      </Link>
+
+      <Link to="/appinsurance/MainArea/Client" className="side-bar-item">
+        {isMinimize ? <LuUser /> : "Clients"}  
+      </Link>
+
+      <Link to="/appinsurance/MainArea/Due" className="side-bar-item">
+        {isMinimize ? <LuCalendarArrowUp /> : "Due"}  
+      </Link>
+
+      <Link to="/appinsurance/MainArea/Policy" className="side-bar-item">
+        {isMinimize ? <LuFolder /> : "Policy"}  
+      </Link>
+
+      <Link to="#" className="side-bar-item">
+        {isMinimize ? "M" : "Monthly Data"}  
+      </Link> 
+
+      <Link to="/appinsurance" className="side-bar-item">
+        {isMinimize ? "L" : "Login"}  
+      </Link>
+      
     </div>
   </div>
   );
